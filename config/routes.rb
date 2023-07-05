63665e268064f47fa2fc46ecaa3dfb1453532667
Ryan Hayden <ryanhayden@Live.com>
2023-07-04 20:48:41 -0400

    revert changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  resources :widgets
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-
-  root 'welcome#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
